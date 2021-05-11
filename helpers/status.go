d535c10180228f603f104299c7842791eecbe3d6
Yash-Garg <yash.garg@outlook.in>
2021-05-12 01:18:00 +0530

    Remove unused group handler
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- helpers/status.go
+++ helpers/status.go
@@ -5,7 +5,3 @@ import "github.com/gofiber/fiber/v2"
 func AliveStatus(ctx *fiber.Ctx) error {
 	return ctx.SendString("Nyaa API v1 // Alive")
 }
-
-func GroupHandler(ctx *fiber.Ctx) error {
-	return ctx.Next()
-}
