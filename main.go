8845d671f4f63ea61b6a3f5cfc325dcd7b69574c
Yash-Garg <yash.garg@outlook.in>
2022-03-21 20:02:39 +0530

    chore: add TLS config, if empty then run without TLS

--- main.go
+++ main.go
@@ -45,5 +45,13 @@ func main() {
 	app.Get("/user/:username", helpers.GetUserUploads)
 	app.Get("/:category/:sub_category?", helpers.GetCategoryTorrents)
 
-	_ = app.Listen(getPort())
+	sslCert := os.Getenv("SSL_CERT")
+	sslKey := os.Getenv("SSL_KEY")
+	port := getPort()
+
+	if sslKey != "" && sslCert != "" {
+		_ = app.ListenTLS(port, sslCert, sslKey)
+	} else {
+		_ = app.Listen(port)
+	}
 }
